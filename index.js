c4a34573220feade9c19f20dbca0176629797081
ratacat <neuralsplash@gmail.com>
2015-05-24 10:40:24 -0700

    /signup route and post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var body = require ("body-parser");
 var path = require("path");
+var ejs = require("ejs");
 
 
 var app = express();
@@ -9,10 +10,26 @@ var views = path.join(__dirname,"views");
 app.use(express.static(path.join(process.cwd(),'public')));
 app.use(body.urlencoded({extended: true}));
 app.get ("/",function(req,res) {
-	res.send("oh yes");
-})
+	var homePath = path.join(views, "home.ejs");
+	console.log(homePath);
+	res.render(homePath);
+});
 
+app.get("/signup", function(req,res) {
+	var signedupPath = path.join(views, "signup.ejs");
+	res.render(signedupPath);
+});
 
+app.post("/users",function(req,res) {
+	var newUser = req.body.user;
+	db.User.createSecure(newUser,function(err,user) {
+		if (user) {
+			res.send(user);
+		} else {
+			res.redirect("/signup");
+		}
+	});
+});
 
 
 app.listen(3000,function() {
