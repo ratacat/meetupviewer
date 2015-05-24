9ed7be1ecefc6742d921611c462d755104a73a03
ratacat <neuralsplash@gmail.com>
2015-05-24 10:14:49 -0700

    response working

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var path = require("path");
 
 
 var app = express();
+var views = path.join(__dirname,"views");
 
+app.use(express.static(path.join(process.cwd(),'public')));
 app.use(body.urlencoded({extended: true}));
 app.get ("/",function(req,res) {
 	res.send("oh yes");
