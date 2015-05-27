9e89adf00dc9ab67bfd41093bf9ce92bcbbd6b7b
ratacat <neuralsplash@gmail.com>
2015-05-27 12:25:00 -0700

    modified render to include information about user follows

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,45 @@
 $(function() {
 	render("eventTemplate","eventsContainer");
+
 });
 
+function sendFollow(event) {
+	var groupId = $(event).data().id;
+	$.ajax({
+		url: "/follow/" + groupId,
+		type: "POST",
+		success: function(res) {
+			console.log("sendFollow callback success! ::"+res);
+		}
+	});
+}
 
 //renders all templates and appends them inside targetId element
 function render(templateId,targetId) {
+	var groups,followed;
 	//console.log("render init");
+	// $.get("/follows").done(function(uGroups) {
+	// 	groups = uGroups;
+	// });
+
 	//get json and build template view
     $.get('/events').done(function(events) {
-    	$("#" + targetId).html('');
-        _.each(events,function(event,index,list) {
-         	//console.log("data: " + element);
-            var template = _.template($("#" + templateId).html());
-           // console.log(template);
-           //first key in object sets the variable in the template
-           console.log("event: " + event);
-            $("#" + targetId).append(template({event: event}));
+    	$.get("/follows").done(function(groups) {
+	    	$("#" + targetId).html('');
+	        _.each(events,function(event,index,list) {
+	         	//console.log("data: " + element);
+	            var template = _.template($("#" + templateId).html());
+	           // console.log(template);
+	           //first key in object sets the variable in the template
+	           //console.log("event: " + event);
+	           console.log("event.groupId: "+ event.groupId);
+	           if (groups.indexOf(event.groupId) > 0) {followed = 'followed'} else {followed=''}
+	            $("#" + targetId).append(template({event: event,f: followed}));
+	        });
+	        //lets add all of the onclick events at once
+			//note: because this requires follow data, if user is not logged in
+			//no data can be returned, and it breaks the whole render
+	        $("div.event div.follow:not(div.followed)").attr("onclick","sendFollow(this)");
         });
 
     });
