5f927bcd67aef54da5eb0622bd18859b2ab6ef0b
ratacat <neuralsplash@gmail.com>
2015-05-28 15:40:02 -0700

    select between databases

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/meetup");
+mongoDb = process.env.MEETUPVIEWER_MONGO || process.env.MONGOLAB_URI
+mongoose.connect(mongoDb);
 module.exports.User = require("./user.js");
 //module.exports.Book = require("./book.js");
